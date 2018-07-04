1481a50521fd9dc93d2536b6ce0be83a04a96b74
Wasipo Repository <pwnlowed@icloud.com>
2018-07-04 14:35:26 +0900

    first

---
+++ js/plugins/SRD_GameUpgrade.js
@@ -0,0 +1,9 @@
+Window_TitleCommand.prototype.makeCommandList = function() {
+ this.addCommand(TextManager.newGamePlus,   newGamePlus')
+    this.addCommand(TextManager.newGame,   'newGame');
+   // this.addCommand(TextManager.continue_, 'continue', this.isContinueEnabled());
+    // this.addCommand(TextManager.options,   'options');
+};
+
+Window_MenuCommand.prototype.addOptionsCommand = function() {
+};
